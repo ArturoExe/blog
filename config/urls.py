d627424687cce4253b753a30abf7e569578d9b01
Arturo Martinez <amarturin15@gmail>
2022-05-07 19:48:09 -0700

    Class2

--- config/urls.py
+++ config/urls.py
@@ -18,7 +18,7 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('pages/',include('pages.urls')),
+    path('',include('pages.urls')),
     path('posts/',include('posts.urls')),
     path('accounts/',include('django.contrib.auth.urls')),
     path('accounts/',include('accounts.urls')),
--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,7 @@
 from django.urls import reverse_lazy
 from django.views.generic import ListView,DetailView,CreateView,UpdateView,DeleteView
 from .models import Post
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin,UserPassesTestMixin
 
 class PostListView(ListView):
     template_name='posts/list.html'
@@ -23,12 +23,20 @@ class PostCreateView(LoginRequiredMixin,CreateView):
         form.instance.author=self.request.user
         return super().form_valid(form)
 
-class PostUpdateView(LoginRequiredMixin,UpdateView):
+class PostUpdateView(LoginRequiredMixin,UserPassesTestMixin,UpdateView):
     template_name='posts/edit.html'
     model=Post
     fields=['title','body']
 
-class PostDeleteView(LoginRequiredMixin,DeleteView):
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author==self.request.user
+
+class PostDeleteView(LoginRequiredMixin,UserPassesTestMixin,DeleteView):
     template_name='posts/delete.html'
     model=Post
     success_url=reverse_lazy('post_list')
+
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author==self.request.user
